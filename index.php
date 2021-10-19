5743f02e054b5cd774314f263a0aea499c311b8d
megnemveszithetek <91388932+megnemveszithetek@users.noreply.github.com>
2021-10-19 12:48:22 +0200

    Update index.php
    na vajpn jó???fas

--- index.php
+++ index.php
@@ -63,6 +63,10 @@ include './csatolt.php';
                     <td><input type="button" id="regisztral" onclick="regisztracio()" value="Regisztráció"></td>
                     <td><input type="button" value="Mégse" onclick="zar('regisztracio')"></td>
                 </tr>
+                <tr>
+                    <td><input type="button" id="regisztral" onclick="regisztracio()" value="Regisztráció"></td>
+                    <td><input type="button" value="Mégse" onclick="zar('regisztracio')"></td>
+                </tr>
                 <!--        dasdasgsdfhsiodfhgaéliud                         -->
             </table>
         </div>
