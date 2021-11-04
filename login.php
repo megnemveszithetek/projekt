93bba9e985da8e3422f611bb7e43b16ac86a098b
megnemveszithetek <91388932+megnemveszithetek@users.noreply.github.com>
2021-11-04 13:10:03 +0100

    menu

--- login.php
+++ login.php
@@ -11,6 +11,49 @@ if (mysqli_num_rows($eredmeny) == 1){
     $_SESSION['vnev']=$rekord['vnev'];
     $_SESSION['knev']=$rekord['knev'];
     echo "<ul id='menu' class='jobbra'><li onclick=\"nyit('kijelentablak')\">".$_SESSION['vnev'].' '.$_SESSION['knev']."</li></ul>";
+    // echo "<!DOCTYPE html>
+    // <html>
+    //   <head>
+    //     <style>
+    //       ul {
+    //         display: inline;
+    //         margin: 0;
+    //         padding: 0;
+    //       }
+    //       ul li {
+    //         display: inline-block;
+    //         cursor: pointer;
+    //       }
+    //       ul li:hover ul {
+    //         display: block;
+    //       }
+    //       ul li ul {
+    //         position: absolute;
+    //         width: 200px;
+    //         display: none;
+    //       }
+    //       ul li ul li {
+    //         display: block;
+    //         cursor: pointer;
+    //       }
+    //     </style>
+    //   </head>
+    //   <body>
+    //     <div>
+    //       <ul>
+    //         <li>
+    //           <a>Felhasználó</a>
+    //           <ul>
+    //             <li>Link 1</li>
+    //             <li>Link 2</li>
+    //             <li>Link 3</li>
+    //           </ul>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </body>
+    // </html>
+    // ";
 } else {
     echo "Hibás bejelntkezés";
 }
