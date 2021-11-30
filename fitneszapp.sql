2366df289e945edf69299d5f40181185f761fd9d
megnemveszithetek <91388932+megnemveszithetek@users.noreply.github.com>
2021-11-30 17:52:06 +0100

    sql

--- fitneszapp.sql
+++ fitneszapp.sql
@@ -1,16 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2021. Nov 30. 17:50
+-- Kiszolgáló verziója: 10.4.16-MariaDB
+-- PHP verzió: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `fitneszapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `adatok`
+--
+
 CREATE TABLE `adatok` (
   `id` int(11) NOT NULL,
   `felhid` int(11) NOT NULL,
   `kg` int(11) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp()
+  `datum` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `baratlista`
+--
 
 CREATE TABLE `baratlista` (
   `baratID` int(11) NOT NULL,
@@ -18,11 +46,23 @@ CREATE TABLE `baratlista` (
   `barat2` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `edzes`
+--
+
 CREATE TABLE `edzes` (
   `mikor` datetime NOT NULL,
   `ID` int(11) NOT NULL,
   `gyakorlat` varchar(20) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `etel`
+--
 
 CREATE TABLE `etel` (
   `nev` varchar(40) COLLATE utf8_hungarian_ci NOT NULL,
@@ -32,6 +72,11 @@ CREATE TABLE `etel` (
   `kaloria` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalo`
+--
 
 CREATE TABLE `felhasznalo` (
   `id` int(11) NOT NULL,
@@ -42,12 +87,25 @@ CREATE TABLE `felhasznalo` (
   `szuldatum` date NOT NULL,
   `neme` tinyint(1) NOT NULL,
   `magassag` int(3) NOT NULL,
-  `loggedin` tinyint(1) NOT NULL
+  `loggedin` tinyint(1) NOT NULL,
+  `jog` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
-INSERT INTO `felhasznalo` (`id`, `vnev`, `knev`, `email`, `pass`, `szuldatum`, `neme`, `magassag`, `loggedin`) VALUES
-(3, 'íyx', 'íyx', 'íyx', '90c6580fd464515dca1aad119fd638b154ed9fe5', '2021-10-03', 0, 352, 1);
+--
+-- A tábla adatainak kiíratása `felhasznalo`
+--
 
+INSERT INTO `felhasznalo` (`id`, `vnev`, `knev`, `email`, `pass`, `szuldatum`, `neme`, `magassag`, `loggedin`, `jog`) VALUES
+(5, 'asd', 'asd', 'asd', 'f10e2821bbbea527ea02200352313bc059445190', '2021-11-12', 1, 412, 0, 1),
+(10, 'qwe', 'qwe', 'qwe', '056eafe7cf52220de2df36845b8ed170c67e23e3', '2021-11-13', 0, 421, 1, 0),
+(11, 'íyx', 'íyx', 'íyx', '90c6580fd464515dca1aad119fd638b154ed9fe5', '2021-11-04', 0, 123, 0, 0),
+(12, 'fgh', 'fgh', 'fgh', '3f7b1e2a98dea15069fdc2542560c21bf5fd8234', '2021-11-18', 1, 643, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `gyakorlat`
+--
 
 CREATE TABLE `gyakorlat` (
   `tipus` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
@@ -56,44 +114,92 @@ CREATE TABLE `gyakorlat` (
   `szeria` varchar(5) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `adatok`
+--
 ALTER TABLE `adatok`
   ADD PRIMARY KEY (`id`),
   ADD KEY `felhid` (`felhid`);
 
+--
+-- A tábla indexei `baratlista`
+--
 ALTER TABLE `baratlista`
   ADD PRIMARY KEY (`baratID`),
   ADD KEY `barat1` (`barat1`),
   ADD KEY `barat2` (`barat2`);
 
+--
+-- A tábla indexei `edzes`
+--
 ALTER TABLE `edzes`
   ADD PRIMARY KEY (`mikor`),
   ADD KEY `ID` (`ID`),
   ADD KEY `gyakID` (`gyakorlat`);
- tábla indexei `felhasznalo`
- 
+
+--
+-- A tábla indexei `felhasznalo`
+--
 ALTER TABLE `felhasznalo`
   ADD PRIMARY KEY (`id`);
 
+--
+-- A tábla indexei `gyakorlat`
+--
 ALTER TABLE `gyakorlat`
   ADD PRIMARY KEY (`gyakorlat`);
 
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `adatok`
+--
 ALTER TABLE `adatok`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT a táblához `baratlista`
+--
 ALTER TABLE `baratlista`
   MODIFY `baratID` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT a táblához `felhasznalo`
+--
 ALTER TABLE `felhasznalo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `adatok`
+--
 ALTER TABLE `adatok`
   ADD CONSTRAINT `adatok_ibfk_1` FOREIGN KEY (`felhid`) REFERENCES `felhasznalo` (`id`);
 
+--
+-- Megkötések a táblához `baratlista`
+--
 ALTER TABLE `baratlista`
   ADD CONSTRAINT `baratlista_ibfk_1` FOREIGN KEY (`barat1`) REFERENCES `felhasznalo` (`id`),
   ADD CONSTRAINT `baratlista_ibfk_2` FOREIGN KEY (`barat2`) REFERENCES `felhasznalo` (`id`);
 
+--
+-- Megkötések a táblához `edzes`
+--
 ALTER TABLE `edzes`
   ADD CONSTRAINT `edzes_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `felhasznalo` (`id`),
   ADD CONSTRAINT `edzes_ibfk_2` FOREIGN KEY (`gyakorlat`) REFERENCES `gyakorlat` (`gyakorlat`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -21,126 +21,127 @@ include './csatolt.php';
     </nav>
     <div id="felhasznalo"></div>
     <!--           Bejelentkezés             -->
-        <div id="bejelentablak">
+    <div id="bejelentablak">
+        <table>
+            <tr>
+                <td colspan="2" id="hibasbej"> Hibás email vagy jelszó</td>
+            </tr>
+            <tr>
+                <td>E-mail cím:</td>
+                <td><input type="text" id="bejemail" onkeyup="ellenoriz()"></td>
+            </tr>
+            <tr>
+                <td>Jelszó:</td>
+                <td><input type="password" id="jelszo" onkeyup="ellenoriz()"></td>
+            </tr>
+            <tr>
+                <td><input type="button" id="bejelgomb" onclick="bejelentkezes()" value="Bejelentkezés" disabled></td>
+                <td><input type="button" value="Mégse" onclick="megse()"></td>
+            </tr>
+        </table>
+    </div>
+<!--           Regisztráció              -->
+    <div id="regisztracio">
+        <table>
+            <tr>
+                <td>Vezeték név:</td>
+                <td><input type="text" id="vnev"></td>
+            </tr>
+            <tr>
+                <td>Kereszt név:</td>
+                <td><input type="text" id="knev"></td>
+            </tr>
+            <tr>
+                <td>E-mail cím:</td>
+                <td><input type="text" id="regemail"></td>
+            </tr>
+            <tr>
+                <td>Jelszó: </td>
+                <td><input type="password" id="pass"></td>
+            </tr>
+            <tr>
+                <td>Jelszó újra:</td>
+                <td><input type="password" id="pass2"></td>
+            </tr>
+            <tr>
+                <td>Születési dátum:</td>
+                <td><input type="date" id="szuldatum"></td>
+            </tr>
+            <tr>
+                <td>Neme: </td>
+                <td><select id="neme">
+                    <option value="0" selected>Nő</option>
+                    <option value="1">Férfi</option>
+                </select></td>
+            </tr>
+            <tr>
+                <td>Magasság: </td>
+                <td><input type="number" id="magas"></td>
+            </tr>
+            <tr>
+                <td><input type="button" id="regisztralas" onclick="regisztracio()" value="Regisztráció"></td>
+                <td><input type="button" value="Mégse" onclick="megse()"></td>
+            </tr>
+        </table>
+    </div>
+<!--           Kijelentkezés             -->
+    <div id="kijelentablak">
+            Biztosan ki akar jelentkezni?<br>
+            <input type="button" onclick="kijelentkezes()" value="Kilépés">
+            <input type="button" value="Mégse" onclick="zar('kijelentablak')">
+    </div>
+<!--           Jelszó csere              -->
+        <div id="jelszocsere">
             <table>
                 <tr>
-                    <td colspan="2" id="hibasbej"> Hibás email vagy jelszó</td>
+                    <td>Régi jelszó:</td>
+                    <td><input type="password" id="oldpass"></td>
                 </tr>
                 <tr>
-                    <td>E-mail cím:</td>
-                    <td><input type="text" id="bejemail" onkeyup="ellenoriz()"></td>
+                    <td>Új jelszó:</td>
+                    <td><input type="password" id="pass1"></td>
                 </tr>
                 <tr>
-                    <td>Jelszó:</td>
-                    <td><input type="password" id="jelszo" onkeyup="ellenoriz()"></td>
+                    <td>Új jelszó mégegyszer:</td>
+                    <td><input type="password" id="pass2" onchange="ellenorzes('pass1', 'pass2', 'ujjelszo')"></td>
                 </tr>
                 <tr>
-                    <td><input type="button" id="bejelgomb" onclick="bejelentkezes()" value="Bejelentkezés" disabled></td>
-                    <td><input type="button" value="Mégse" onclick="megse()"></td>
+                    <td>
+                        <input type="button" onclick="changepassword()" id="ujjelszo" value="Cserél" disabled>
+                    </td>
+                    <td>
+                        <input type="button" value="Mégse" onclick="zar('jelszocsere')">
+                    </td>
                 </tr>
             </table>
         </div>
-    <!--           Regisztráció              -->
-        <div id="regisztracio">
+        <div id="uzenet"></div>
+<!--           Profil                    -->
+        <div id="profil">
             <table>
-                <tr>
-                    <td>Vezeték név:</td>
-                    <td><input type="text" id="vnev"></td>
-                </tr>
-                <tr>
-                    <td>Kereszt név:</td>
-                    <td><input type="text" id="knev"></td>
-                </tr>
-                <tr>
-                    <td>E-mail cím:</td>
-                    <td><input type="text" id="regemail"></td>
-                </tr>
-                <tr>
-                    <td>Jelszó: </td>
-                    <td><input type="password" id="pass"></td>
-                </tr>
-                <tr>
-                    <td>Jelszó újra:</td>
-                    <td><input type="password" id="pass2"></td>
-                </tr>
-                <tr>
-                    <td>Születési dátum:</td>
-                    <td><input type="date" id="szuldatum"></td>
-                </tr>
-                <tr>
-                    <td>Neme: </td>
-                    <td><select id="neme">
-                        <option value="0" selected>Nő</option>
-                        <option value="1">Férfi</option>
-                    </select></td>
-                </tr>
-                <tr>
-                    <td>Magasság: </td>
-                    <td><input type="number" id="magas"></td>
-                </tr>
-                <tr>
-                    <td><input type="button" id="regisztralas" onclick="regisztracio()" value="Regisztráció"></td>
-                    <td><input type="button" value="Mégse" onclick="megse()"></td>
-                </tr>
+                <tr><th>Vezeték Név</th><th>Kereszt név</th><th>Email cím</th><th>Születési dátum</th><th>Neme</th><th>Magasság</th></tr>
+                <?php
+                    $user="SELECT vnev, knev, email, szuldatum, neme, magassag FROM felhasznalo WHERE id=".$_SESSION['id'];
+                    $eredmeny1=mysqli_query($kapcsolat, $user);
+                    while($adatsor1=mysqli_fetch_array($eredmeny1)){
+                        echo "<tr><td>".$adatsor1['vnev']."</td><td>".$adatsor1['knev']."</td><td>".$adatsor1['email']."</td><td>".$adatsor1['szuldatum']."</td><td>".$adatsor1['neme']."</td><td>".$adatsor1['magassag']."</td></tr>";
+                    }
+                ?>
             </table>
         </div>
-    <!--           Kijelentkezés             -->
-        <div id="kijelentablak">
-             Biztosan ki akar jelentkezni?<br>
-             <input type="button" onclick="kijelentkezes()" value="Kilépés">
-             <input type="button" value="Mégse" onclick="zar('kijelentablak')">
+<!--           Adminpanel felhasználók   -->
+        <div id="users">
+            <table>
+                <tr><th>ID</th><th>Vezeték Név</th><th>Kereszt név</th><th>Email cím</th><th>Születési dátum</th><th>Neme</th><th>Magasság</th><th>jog</th></tr>
+                <?php
+                    $sqlmondat="SELECT * FROM felhasznalo";
+                    $eredmeny=mysqli_query($kapcsolat, $sqlmondat);
+                    while($adatsor=mysqli_fetch_array($eredmeny)){
+                        echo "<tr><td>".$adatsor['id']."</td><td>".$adatsor['vnev']."</td><td>".$adatsor['knev']."</td><td>".$adatsor['email']."</td><td>".$adatsor['szuldatum']."</td><td>".$adatsor['neme']."</td><td>".$adatsor['magassag']."</td><td>".$adatsor['jog']."</td></tr>";
+                    
+                    }
+                ?>
+            </table>
         </div>
-    <!--           Jelszó csere              -->
-            <div id="jelszocsere">
-                <table>
-                    <tr>
-                        <td>Régi jelszó:</td>
-                        <td><input type="password" id="oldpass"></td>
-                    </tr>
-                    <tr>
-                        <td>Új jelszó:</td>
-                        <td><input type="password" id="pass1"></td>
-                    </tr>
-                    <tr>
-                        <td>Új jelszó mégegyszer:</td>
-                        <td><input type="password" id="pass2" onchange="ellenorzes('pass1', 'pass2', 'ujjelszo')"></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="button" onclick="changepassword()" id="ujjelszo" value="Cserél" disabled>
-                        </td>
-                        <td>
-                            <input type="button" value="Mégse" onclick="zar('jelszocsere')">
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div id="uzenet"></div>
-    <!--           Profil                    -->
-            <div id="profil">
-                <table>
-                    <tr><th>Vezeték Név</th><th>Kereszt név</th><th>Email cím</th><th>Születési dátum</th><th>Neme</th><th>Magasság</th></tr>
-                    <?php
-                        $user="SELECT vnev, knev, email, szuldatum, neme, magassag FROM felhasznalo WHERE id=".$_SESSION['id'];
-                        $eredmeny1=mysqli_query($kapcsolat, $user);
-                        while($adatsor1=mysqli_fetch_array($eredmeny1)){
-                            echo "<tr><td>".$adatsor1['vnev']."</td><td>".$adatsor1['knev']."</td><td>".$adatsor1['email']."</td><td>".$adatsor1['szuldatum']."</td><td>".$adatsor1['neme']."</td><td>".$adatsor1['magassag']."</td></tr>";
-                        }
-                    ?>
-                </table>
-            </div>
-    <!--           Adminpanel felhasználók   -->
-            <div id="users">
-                <table>
-                    <tr><th>ID</th><th>Vezeték Név</th><th>Kereszt név</th><th>Email cím</th><th>Születési dátum</th><th>Neme</th><th>Magasság</th><th>jog</th></tr>
-                    <?php
-                        $sqlmondat="SELECT * FROM felhasznalo";
-                        $eredmeny=mysqli_query($kapcsolat, $sqlmondat);
-                        while($adatsor=mysqli_fetch_array($eredmeny)){
-                            echo "<tr><td>".$adatsor['id']."</td><td>".$adatsor['vnev']."</td><td>".$adatsor['knev']."</td><td>".$adatsor['email']."</td><td>".$adatsor['szuldatum']."</td><td>".$adatsor['neme']."</td><td>".$adatsor['magassag']."</td><td>".$adatsor['jog']."</td></tr>";
-                        }
-                    ?>
-                </table>
-            </div>
 </body>
 </html>
