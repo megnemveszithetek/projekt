ad4712af969474f9ee911e1bd4f723bad0142a26
megnemveszithetek <91388932+megnemveszithetek@users.noreply.github.com>
2022-01-31 07:44:18 +0100

    register

---
+++ mobil/Node/dbrepo.js
@@ -0,0 +1,27 @@
+module.exports.login = function (conn) {
+    return new Promise((reject, resolve) => {
+        conn.query("select * from felhasznalo", (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+
+        });
+    });
+}
+
+module.exports.register = function (conn, felhasznalo) {
+    return new Promise((reject, resolve) => {
+        var salt = bcrypt.genSaltSync(10);
+        var pass = bcrypt.hashSync(felhasznalo.pass, salt);
+        conn.query("INSERT INTO felhasznalo(id, vnev, knev, email, pass, szuldatum, neme, magassag, loggedin) VALUES (0,?,?,?,?,?,?,?,false)", [felhasznalo.vnev, felhasznalo.knev, felhasznalo.email, felhasznalo.pass, felhasznalo.szuldatum, felhasznalo.neme, felhasznalo.magassag], (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({ message: "Adat módosítva", status: "201" });
+            }
+
+        });
+    });
+}
--- mobil/Node/index.js
+++ mobil/Node/index.js
@@ -1,20 +1,36 @@
-const express =require('express')
-const cors=require('cors');
-const mysql=require('mysql');
-const app = express()
-const conn=mysql.createConnection(
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const app = express();
+const { login, register } = require('./dbrepo');
+
+const conn = mysql.createConnection(
     {
-        host:"localhost",
-        user:"root",
-        password:"",
-        database:"fitnesapp"
-    
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "fitneszapp"
+
     });
 
-app.get('/', (req, res) =>{
-     res.render('index.js')
-})
 
-app.listen(3000,() =>{
-    console.log('A server fut')
-})
+app.use(cors());
+
+
+app.use(express.json({ extended: true }));
+
+app.listen(3000, () => { console.log("Server running") });
+
+
+app.get('/login', async (req, res) => {
+    login(conn)
+        .then(results => res.json(results))
+        .catch(err => res.send(err));
+});
+
+app.post('/register', async (req, res) => {
+    register(conn, req.body)
+        .then(results => res.json(results))
+        .catch(err => res.send(err));
+});
